a168fca279b80d7d0f0107bacffe6e00302abae7
Sahil Kumar <sahilkumarlpu1@gmail.com>
2025-07-12 09:35:06 +0530

    created db

---
+++ backend/database/connection.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "AC_PLATFORM",
+    });
+    console.log("Connected to the database.");
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err.message}`);
+    process.exit(1);
+  }
+};
